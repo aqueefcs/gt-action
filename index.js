8f920a96a6311cc0dd857b64747c5ce64a84b907
aqueefcs <aqueefalimansari007@gmail.com>
2023-10-16 20:17:52 +0530

    Add logic

---
+++ index.js
@@ -0,0 +1,77 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+  try {
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const pr_number = core.getInput('pr_number', { required: true })
+    const token = core.getInput('token', { required: true })
+
+    const octokit = new github.getOctokit(token)
+
+    const { data: changedFiles } = await octokit.rest.pull.listFiles({
+      owner,
+      repo,
+      pull_number: pr_number
+    })
+
+    let diffData = {
+      additions: 0,
+      deletions: 0,
+      changes: 0
+    }
+
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions
+      acc.deletions += file.deletions
+      acc.changes += file.changes
+
+      return acc
+    }, diffData)
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+            Pull request ${pr_number} has been updated with : \n
+            - ${diffData.additions} additions \n 
+            - ${diffData.deletions} deletions \n 
+            - ${diffData.changes} changes 
+        `
+    })
+
+    for (const file of changedFiles) {
+      const fileExtension = file.filename.split('.').pop()
+      let label = ''
+      switch (fileExtension) {
+        case 'md':
+          label = 'markdown'
+          break
+        case 'js':
+          label = 'javascript'
+          break
+        case 'yml':
+          label = 'yaml'
+          break
+        case 'yaml':
+          label = 'yaml'
+          break
+        default:
+          label = 'noextention'
+      }
+
+      await octokit.rest.issues.addLabel({
+        owner,
+        repo,
+        issue_number: pr_number,
+        labels: [label]
+      })
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
